7fbb68ce30c659b670c2289599262b2c8eb528dd
G.Modiriran <Ghodrat.Modiririan@gmail.com>
2022-09-13 12:14:33 +0300

    Add static cashable picture 8

--- myblog02/main/models.py
+++ myblog02/main/models.py
@@ -1,8 +1,8 @@
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-'''
+
 class BlogTag(models.Model):
     name = models.CharField('Name', max_length=60, blank=False, null=False)
     slug = models.SlugField('Slug', editable=False, max_length=180, unique=True, default=timezone.now)
@@ -13,21 +13,19 @@ class BlogTag(models.Model):
     def save(self, *args, **kwargs):
         self.slug = str(self.name).strip().replace(' ', '-')
         super().save(*args, **kwargs)
-'''
+
 
 # Create your models here.
 
 class BlogPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False,
+                            verbose_name='Author')
     title = models.CharField('Title', max_length=120, blank=False, null=False) 
     slug = models.SlugField('Slug', editable=False, max_length=180, unique=True, default=timezone.now)
     content = models.TextField('Content', max_length=800, blank=False, null=False)
-    
-    #slug = models.SlugField('Slug', editable=False, max_length=180, unique=True, default=timezone.now)
-       #banner = models.ImageField('Image', upload_to='images/', blank=True,
-    #                             null=True, default='images/default_image.png')
-    #user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False,
-    #                        verbose_name='Author')
-    #tags = models.ManyToManyField(BlogTag, verbose_name='Tags')
+    banner = models.ImageField('Image', upload_to='images/', blank=True,
+                                 null=True, default='images/default_image.png')
+    tags = models.ManyToManyField(BlogTag, verbose_name='Tags')
     
     def __str__(self):
         return self.title
