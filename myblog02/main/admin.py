f5c80240575a5724b1dd9c9b9ab0b5ef04775d41
G.Modiriran <Ghodrat.Modiririan@gmail.com>
2022-09-13 09:21:36 +0300

    admin activation 6

--- myblog02/main/admin.py
+++ myblog02/main/admin.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import Group
 from django.contrib import admin
+from.models import BlogPost
 
 # Register your models here.
+admin.site.register(BlogPost)
+
+# Unregister your models here.
+admin.site.unregister(Group)
+
+
