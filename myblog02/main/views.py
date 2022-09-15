08097ee43612b4bc936ddc8b5807e76c0e9a0961
G.Modiriran <Ghodrat.Modiririan@gmail.com>
2022-09-15 09:43:50 +0300

    35 -05-LoadPostsImages 12

--- myblog02/main/views.py
+++ myblog02/main/views.py
@@ -1,6 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import BlogTag, BlogPost
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    #model = BlogTag
     template_name= 'index.html'
+    #context_object_name = 'tags'
+
+    def get_queryset(self, *args, **kwargs):
+        query = BlogPost.objects.all().order_by('-pk')
+        return query
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['posts'] = self.get_queryset()
+        context['tags'] = BlogTag.objects.all()
+        return context
 
 # Create your views here.
